732b7235ffbce4dcd8969a5660ba2cfebc6a8248
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2024-05-07 17:46:15 +0700

    Pointer Object mhs

--- PointerdanFriendclass_0050.cpp
+++ PointerdanFriendclass_0050.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 class mahasiswa {
-	public;
+public:
 	int nim;
 	void showNim() {
 		cout << "No Induk = " << nim << endl;
@@ -11,9 +11,15 @@ class mahasiswa {
 
 int main() {
 
-	mahasiswa mhs(1); // Object mhs
+	mahasiswa mhs{ 1 }; // Object mhs
 	mhs.showNim(); // Member Access Operator
 
 	mahasiswa& refMhs = mhs; // Pointer References refMhs
 	refMhs.nim = 2; // Member Access Operator
+	mhs.showNim();
+
+	mahasiswa* pMhs = &mhs; // Pointer Dereferences pMhs
+	pMhs->nim = 3; // Arrow Operator
+	pMhs->showNim();
+	return 0;
 }
